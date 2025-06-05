20a579b19879ab141c595a91620c402d62dc4b5e
Fedor Ptitsyn <fedor.ptith@gmail.com>
2025-06-05 23:43:00 +0300

    help

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,31 @@
+﻿from django.contrib import admin
+from .models import Category, Location, Post, Comment
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_published')
+    list_editable = ('is_published',)
+    search_fields = ('title',)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published')
+    list_editable = ('is_published',)
+    search_fields = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'author', 'is_published')
+    list_editable = ('is_published',)
+    search_fields = ('title', 'text')
+    list_filter = ('category', 'location', 'is_published')
+    date_hierarchy = 'pub_date'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'post', 'author', 'created_at')
+    search_fields = ('text', 'author__username', 'post__title')
+    list_filter = ('created_at',)
---
+++ blogicum/blog/functions.py
@@ -0,0 +1,27 @@
+﻿from django.db import models
+from django.db.models import Count
+from django.core.paginator import Paginator
+from django.utils import timezone
+
+
+def annotate_posts_with_comments(query_set):
+    return query_set.annotate(
+        comment_count=Count('comments')
+    ).order_by('-pub_date')
+
+
+def get_paginated_page(request, query_set, per_page=10):
+    paginator = Paginator(query_set, per_page)
+    page_number = request.GET.get('page')
+    return paginator.get_page(page_number)
+
+
+def filter_published_posts(query_set, user=None):
+    query_set = query_set.filter(
+        category__is_published=True,
+        pub_date__lte=timezone.now()
+    )
+    if not user or not user.is_authenticated:
+        return query_set.filter(is_published=True)
+    return query_set.filter(
+        models.Q(is_published=True) | models.Q(author=user))
